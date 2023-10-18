bbe750aba1dac553c654b63cdbb102def0fd4e88
Syed Taseer Shah <syedtaseershah@Syeds-MacBook-Pro.local>
2023-10-18 12:12:25 +0500

    new basic api

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.post('/api', protect, (req, res) => {
     res.send({ greetings: 'you are logged in!'});
 });
 
+app.post('/api/user', protect, (req, res) => {
+    res.send({ name: `Logged in as user1`});
+});
+
+
 app.post('/user', createUser);
 
 module.exports = app;
